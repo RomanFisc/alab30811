f4ebfed347f70c728fdf993c0c230536d75687be
RomanFisc <romanfisc@gmail.com>
2024-05-21 11:39:28 -0400

    have let, const, arithimatic operators, comparison operators, and string concats

---
+++ lab30811.js
@@ -0,0 +1,15 @@
+const person = "John"
+let y = 7
+let x = 5
+
+console.log(person + y);
+
+if (x>y) {
+  console.log("X is bigger")
+} else {
+  console.log("Y is bigger")
+}
+
+const hello = "Hello, "
+const greeting = "how are you?"
+console.log(hello.concat(greeting))
